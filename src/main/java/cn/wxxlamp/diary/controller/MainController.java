0d1113c6d698602d0bb511a5ea9010031a5b2acf
wxx <wxxlamp@foxmail.com>
2021-10-04 21:17:40 +0800

    完成组件相对位置

--- src/main/java/cn/wxxlamp/diary/controller/MainController.java
+++ src/main/java/cn/wxxlamp/diary/controller/MainController.java
@@ -6,9 +6,12 @@ import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
 import java.io.File;
 import java.net.URL;
@@ -23,13 +26,22 @@ import static cn.wxxlamp.diary.constants.FxmlComponents.WRITER_PANE;
 public class MainController implements Initializable {
 
     @FXML
-    JFXButton writerButton;
+    private JFXButton writerButton;
 
     @FXML
-    AnchorPane writerPane;
+    private AnchorPane writerPane;
 
     @FXML
-    TreeView<String> treeView;
+    private TreeView<String> treeView;
+
+    @FXML
+    private ScrollPane scrollPane;
+
+    @FXML
+    private DatePicker datePicker;
+
+    @FXML
+    private VBox menuBox;
 
     @FXML
     public void write() {
@@ -37,11 +49,34 @@ public class MainController implements Initializable {
         WriterPaneController writerPaneController = loader.getController();
         writerPaneController.getTab().setText("今日日记");
         writerPane.getChildren().add(loader.getRoot());
+        writerButton.setDisable(true);
+        writerButton.setOpacity(0);
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // 加载日记记录
+        initCustomProperties();
+        initDir();
+
+    }
+
+    /**
+     * 绑定尺寸
+     */
+    private void initCustomProperties() {
+//        menuBox.prefWidthProperty().bind(writerButton.widthProperty().divide(7).multiply(3));
+        writerButton.prefWidthProperty().bind(menuBox.widthProperty());
+        writerButton.prefHeightProperty().bind(menuBox.heightProperty().multiply(0.06));
+        datePicker.prefHeightProperty().bind(menuBox.heightProperty().multiply(0.06));
+        datePicker.prefWidthProperty().bind(menuBox.widthProperty());
+        treeView.prefHeightProperty().bind(menuBox.heightProperty().multiply(0.54));
+        scrollPane.prefHeightProperty().bind(menuBox.heightProperty().multiply(0.34));
+    }
+
+    /**
+     * 加载日记
+     */
+    private void initDir() {
         TreeItem<String> rootItem = new TreeItem<>("ROOT");
         PathUtils.getSubFileName(PathUtils.getDir()).forEach(y -> {
             TreeItem<String> yearItem = new TreeItem<>(y + "年");
